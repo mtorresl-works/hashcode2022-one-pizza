00a054c8c5b7d0556f1b4b9a8621b67396e29807
Mateo <matulni24@gmail.com>
2022-01-23 20:35:14 +0100

    Fixed submission function: int for NIngr

--- marina.py
+++ marina.py
@@ -139,7 +139,7 @@ def pizza_chain_to_outstring(pizzaChain, NIng, ingrList):
     :param ingrList: a list of strings with all the ingredients.
     :return: the output file content in one line
     """
-    otr = str(sum(pizzaChain))
+    otr = str(int(sum(pizzaChain)))
     for idx, spin in enumerate(pizzaChain):  # idx position in chain, spin True or False
         if spin: otr = otr + ' ' + ingrList[idx]
     return otr + "\n"
