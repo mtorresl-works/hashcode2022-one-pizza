4c57f2f104008fa96f92a6b78e67d1b2369bae79
Mateo <matulni24@gmail.com>
2022-01-27 13:24:53 +0100

    partition_graph.py

--- partition_graph.py
+++ partition_graph.py
@@ -1,17 +1,32 @@
 import argparse
 import igraph as ig
-import data_conversion
+import utils.data_conversion as data_conversion
+import utils.read_write as read_write
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 if __name__ == '__main__':
 
     flags = data_conversion.read_flags()
+    fId = flags.fId
 
-    nNodesAtc = 1703
     ### GRAPH DEFINITION
-    g = data_conversion.read_graph(flags.fId)
+    gRaw = read_write.read_graph(fId, anticlique=False, NVertex = 4986)
+    gAnti = read_write.read_graph(fId, anticlique=True, NVertex = 2059)
 
-    anticliqueIdx = data_conversion.read_anticlique(flags.fId, nNodesAtc)
-    g.delete_vertices(anticliqueIdx)
-    data_conversion.save_graph(flags.fId + '-' + str(len(g.vs)), g)
+    idsAnti = [c.id for c in gAnti.vs['client_info']]
+
+    quots = []
+    ls = []
+    deg = []
+    for i in range(max(gRaw.vs.degree())):
+        d = len(gRaw.vs.select(_degree=i))
+        if not d==0:
+            l = len(gRaw.vs[idsAnti].select(_degree=i))
+            quots.append(l/d)
+            deg.append(i)
+            ls.append(l)
+
+    plt.plot(deg, ls, 'o-')
+    plt.show()
